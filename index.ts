b746f6ae249242a2f4e44b646dc98631e6c4da16
kzzz-jpg <211096@stu.cchs.chc.edu.tw>
2026-05-21 11:06:16 +0800

    testcase

--- index.ts
+++ index.ts
@@ -290,7 +290,7 @@ async function zerojudgeChecker(config) {
 
 export default class ImportJsonService extends Service {
     static Config = Schema.object({
-    ZjBaseUrl: Schema.string().description('Author Statistic Base URL').default(""),
+    ZjBaseUrl: Schema.string().description('Author Statistic Base URL').default("judge.cchs.chc.edu.tw/"),
     zjCheckerType: Schema.string().description('Zerojudge Checker Type Name').default("qduoj"),
     });
     constructor(ctx: Context, config: ReturnType<typeof ImportJsonService.Config>) {
